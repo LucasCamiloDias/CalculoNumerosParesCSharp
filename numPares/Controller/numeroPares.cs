b48f28d141a453e7d2c2cde49ea66e93b9de2512
lucas.dcamilo <lucas.dcamilo@SBC0505237W10-1>
2023-06-15 09:17:11 -0300

    Adicionar arquivos de projeto.

---
+++ numPares/Controller/numeroPares.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace numPares.Controller
+{
+    internal class numeroPares
+    {
+        public int i = 0;
+        public string res = "";
+        public numeroPares() { }
+        public string Calcular(string v)
+        {
+            res = "";
+            int i = Convert.ToInt32(v);
+                 
+            while (i > 0)
+            { 
+                
+                if (i % 2 == 0)
+                {
+         
+                    i = i - 2; 
+                    res +=  Convert.ToString(i) + ", ";
+                }
+                else
+                {
+                    i = i - 1;
+                    res += Convert.ToString(i) + ", ";
+                }
+            }
+           
+            return res;
+        }
+    }
+}
---
+++ numPares/View/FormPares.cs
@@ -0,0 +1,29 @@
+﻿using numPares.Controller;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace numPares.View
+{
+    public partial class FormPares : Form
+    {
+        
+        public FormPares()
+        {
+            InitializeComponent();
+        }
+         numeroPares pares = new numeroPares();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "";
+            string v  = textBox1.Text;
+            label1.Text = pares.Calcular(v).ToString();
+        }
+    }
+}
